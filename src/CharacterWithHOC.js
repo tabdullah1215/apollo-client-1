4b820a0739a9185fd7be661f2b7ad3e5a10893c2
Tariq Abdullah <tarabdul1@publicisgroupe.net>
2020-01-03 01:48:17 -0500

    add 3 query implementations:  HOC, hook, render

---
+++ src/CharacterWithHOC.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { gql } from 'apollo-boost';
+import { withApollo } from 'react-apollo';
+
+const GET_CHARACTERS = gql`
+    query getCharacters {
+        characters {
+            results {
+                id
+                name
+                image
+            }
+        }
+    }
+`;
+
+function CharacterWithHOC({client}) {
+    const [characters, setCharacters] = useState([]);
+    client.query({ query: GET_CHARACTERS })
+        .then(res => setCharacters(res.data.characters.results))
+        .catch(err => console.log(err));
+    console.log(characters.length);
+    if (characters.length > 0) {
+        return (
+            <div className="characters">
+                {characters.map(character => (
+                    <div key={character.name} className="character">
+                        <img src={character.image} alt={character.name} />
+                        <p>{character.name}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    return (
+        <div className="App">
+            <h2>Loading...</h2>
+        </div>
+    );
+}
+
+export default withApollo(CharacterWithHOC);
---
+++ src/CharacterWithHook.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from 'react-apollo';
+
+const GET_CHARACTERS = gql`
+    query getCharacters {
+        characters {
+            results {
+                id
+                name
+                image
+            }
+        }
+    }
+`;
+
+function CharacterWithHook() {
+    /*****************************************************************/
+    const { loading, error, data } = useQuery(GET_CHARACTERS);
+    /* *****************************************************************/
+    if(error) {
+        return <div>Error!</div>;
+    }
+    if(loading) {
+        return (
+            <div className="App">
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
+    if(data) {
+        if(data.characters.results.length > 0) {
+            return (
+                <div className="characters">
+                    {data.characters.results.map(character => (
+                        <div key={character.name} className="character">
+                            <img src={character.image} alt={character.name} />
+                            <p>{character.name}</p>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+    }
+}
+
+export default CharacterWithHook;
